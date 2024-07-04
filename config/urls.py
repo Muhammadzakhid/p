f969e83694f4c67c938900e59d216efd28864ff3
Muhammadzakhid <m92817516@gmail.com>
2024-07-04 19:53:32 +0500

    Problems fixed

--- config/urls.py
+++ config/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
 ]
 
 # Serve static and media files in development (optional for production)
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'main' 
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Add 'main:' as the namespace prefix
+    path('', views.home, name='home'), 
 ]
